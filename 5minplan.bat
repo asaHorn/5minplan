022be388ed1e46ec31e3f454fc3c08116f08d88b
asaHorn <32079175+asaHorn@users.noreply.github.com>
2021-09-29 20:19:13 -0400

    Added some echos to the end to remind user of rest

--- 5minplan.bat
+++ 5minplan.bat
@@ -1,6 +1,12 @@
 @echo off
+
+&::To whoever on white team looks at this: I may change the passwords, acount names and system paths from their current values.
+
 net user Administrator blueteamrocks &::quickly change default password
 net user guest blueteambestteam      &::quickly change default password
+
+wmic useraccount where name='Administrator' rename 22705adminHIREDTEAM  &::Change default account names just to mess with redteam
+wmic useraccount where name='Administrator' rename 90383guestPLZNOHACK
 
 netsh  advfirewall set allprofiles state on &::turn on firewall
 
@@ -22,3 +28,14 @@ REG ADD  “HKLM\Software\policies\Microsoft\Windows NT\DNSClient”           &
 REG ADD  “HKLM\Software\policies\Microsoft\Windows NT\DNSClient” /v ”EnableMulticast” /t REG_DWORD /d “0” /f
 
 net user  &::Print out the users so user can change all the default passwords
+
+&::START SYSMON HERE
+
+echo "remember to change these passwords"
+echo "remember to do set up for this machine"
+echo "You may find the fllowing sysinternals things useful: Process exporer (enable sig verification in settings), autoruns, Process Manager, tcpview"
+echo "IF THIS IS A DNS MACHINE MAKE 7 BACKUPS IN 20 DIFFRENT PLACES: Windows\System32\DNS Directory"
+echo "alias robocopy <file> to some location you need admin perms to delete (not sure about this one)"
+echo "check group policy. For the most part enabled things are sus"
+echo "check PAM"
+
