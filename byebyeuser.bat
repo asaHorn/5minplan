91b2d1f0670c6c8d28ab49c8a57aded24c9be82b
asaHorn <32079175+asaHorn@users.noreply.github.com>
2021-11-19 16:32:20 -0500

    Create byebyeuser.bat

---
+++ byebyeuser.bat
@@ -0,0 +1,10 @@
+@echo off
+net user "Sharon Webber" /active:no
+net user "Enzo De Surfer" /active:no
+net user "Kalya Shredsalot" /active:no
+
+net user "Micheal Ho" /active:no
+net user "Frieda Zamba" /active:no
+net user "Andy Irons" /active:no
+net user "Margo Oberg" /active:no
+net user "Duke Kanhanamoku" /active:no
